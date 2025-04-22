278485062bbf1d491f765581d8995df1c6b68124
logica0419 <logica0419@gmail.com>
2025-04-23 04:37:28 +0900

    internationalize top page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -13,7 +13,7 @@ const Page = async ({
   const t = await getTranslations("index");
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -24,46 +24,25 @@ const Page = async ({
           priority={true}
         />
         <p>
-          Current Language: <b>{locale}</b>
+          {t("現在の言語")}: <b>{locale}</b>
         </p>
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            {t.rich("さあ、編集して始めましょう。", {
+              code: (children) => (
+                <code
+                  className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold"
+                  style={cica.style}
+                >
+                  {children}
+                </code>
+              ),
+            })}
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            {t("保存するだけで、すぐに変更を確認できます。")}
           </li>
         </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
@@ -79,7 +58,7 @@ const Page = async ({
             width={16}
             height={16}
           />
-          Learn
+          {t("学ぶ")}
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -94,7 +73,7 @@ const Page = async ({
             width={16}
             height={16}
           />
-          Examples
+          {t("実装例を見る")}
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -109,7 +88,7 @@ const Page = async ({
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          {t("nextjs_orgへ")}
         </a>
       </footer>
     </div>
