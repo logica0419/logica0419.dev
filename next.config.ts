ab25c061b6f89cd35a8fe7413ca8b957b4e407cd
logica0419 <logica0419@gmail.com>
2025-04-23 15:49:55 +0900

    enable inline css

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   output: "export",
   allowedDevOrigins: ["localhost", "127.0.0.1"],
+  experimental: { inlineCss: true },
 };
 
 export default withExportImages(createNextIntlPlugin()(nextConfig));
