07eb4ee45ea2263c8c536d6f4516f6dffd16c354
logica0419 <contact@logica0419.dev>
2025-04-30 00:47:14 +0900

    enable plugins both in turbopack and webpack

--- contents/articles/en/test/article.md
+++ contents/articles/en/test/article.md
@@ -1,3 +1,11 @@
+---
+title: Test Article
+emoji: "😴"
+type: "idea" # tech: 技術記事 / idea: アイデア
+topics: ["ISUCON"]
+published: true
+---
+
 # Test.md
 
 This is a test file.
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
+import createMdxPlugin from "@next/mdx";
 import type { NextConfig } from "next";
 import withExportImages from "next-export-optimize-images";
 import createNextIntlPlugin from "next-intl/plugin";
+
+const mdxPluginOptions = {
+  remarkPlugins: [
+    ["remark-gfm"],
+    ["remark-frontmatter", ["yaml"]],
+    ["remark-mdx-frontmatter"],
+    ["remark-breaks"],
+  ],
+  rehypePlugins: [
+    ["@shikijs/rehype", { themes: { light: "light-plus", dark: "dark-plus" } }],
+  ],
+};
 
 const nextConfig: NextConfig = {
   output: "export",
@@ -14,19 +27,8 @@ const nextConfig: NextConfig = {
           {
             loader: require.resolve("@next/mdx/mdx-js-loader.js"),
             options: {
+              ...mdxPluginOptions,
               providerImportSource: "@/mdx-components.tsx",
-              remarkPlugins: [
-                ["remark-gfm"],
-                ["remark-frontmatter", ["yaml"]],
-                ["remark-mdx-frontmatter"],
-                ["remark-breaks"],
-              ],
-              rehypePlugins: [
-                [
-                  "@shikijs/rehype",
-                  { themes: { light: "light-plus", dark: "dark-plus" } },
-                ],
-              ],
             },
           },
         ],
@@ -37,4 +39,11 @@ const nextConfig: NextConfig = {
 };
 
 const withIntl = createNextIntlPlugin();
-export default withExportImages(withIntl(nextConfig));
+const withMdx = createMdxPlugin({
+  extension: /\.mdx?$/,
+  //@ts-ignore
+  options: {
+    ...mdxPluginOptions,
+  },
+});
+export default withExportImages(withIntl(withMdx(nextConfig)));
