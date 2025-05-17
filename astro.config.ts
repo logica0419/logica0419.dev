1f2a575b060d331bd12272bb0796f18b6c6bfca6
Takuto Nagami <logica0419@gmail.com>
2025-05-18 03:32:25 +0900

    inline css

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import sitemap from "@astrojs/sitemap";
+import playformInline from "@playform/inline";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://logica0419.dev",
-  integrations: [sitemap()],
+  integrations: [sitemap(), playformInline()],
   vite: {
     plugins: [tailwindcss()],
   },
