9511510c38726df8a49bad77c290beb8a9924b3b
Takuto Nagami <logica0419@gmail.com>
2025-05-18 15:48:45 +0900

    configure markdown plugins

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,18 @@ import sitemap from "@astrojs/sitemap";
 import playformInline from "@playform/inline";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import remarkLinkCard from "remark-link-card-plus";
 
 export default defineConfig({
   site: "https://logica0419.dev",
   integrations: [sitemap(), playformInline()],
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      themes: { light: "light-plus", dark: "dark-plus" },
+    },
+    remarkPlugins: [[remarkLinkCard, { cache: true }]],
   },
 });
