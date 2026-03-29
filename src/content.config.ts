88ac875fa3caf164383f2713ad0363c46a7a2521
Takuto Nagami <logica0419@gmail.com>
2026-03-30 01:16:45 +0900

    remove tailwind and moved to raw css
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,12 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const articles = defineCollection({
   loader: glob({ pattern: "**/article.md", base: "./articles" }),
   schema: z.object({
     title: z.string(),
-    date: z.string().date().default("1970-01-01"),
+    date: z.iso.date().default("1970-01-01"),
     published: z.boolean().default(true),
   }),
 });
